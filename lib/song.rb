5f0f31be68137fcd374da0edad18482318f5df5c
Stephen McKeon <stephen.mckeon@powerhrg.com>
2020-05-06 12:12:26 -0400

    Makes songs

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -8,6 +10,52 @@ class Song
 
   def save
     self.class.all << self
+    self
   end
 
+  def self.create
+    new.save
+  end
+
+  def self.new_by_name(name)
+    song = new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = create
+    song.name = name
+    song
+  end
+
+  def self.find_by_name(name)
+    all.find { |song| song.name == name }
+  end
+
+  def self.find_or_create_by_name(name)
+    find_by_name(name) || create_by_name(name)
+  end
+
+  def self.alphabetical
+    all.sort_by(&:name)
+  end
+
+  def self.new_from_filename(file)
+    file_array = file.split(" - ")
+    file_array[1].delete_suffix!(".mp3")
+
+    song = new
+    song.artist_name = file_array[0]
+    song.name = file_array[1]
+    song
+  end
+
+  def self.create_from_filename(file)
+    new_from_filename(file).save
+  end
+
+  def self.destroy_all
+    @@all = []
+  end
 end
